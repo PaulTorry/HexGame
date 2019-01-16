71b1f0ff6573a8d28339645439801c4f82400bb7
Paul T <PaulTorry@users.noreply.github.com>
2019-01-15 21:12:13 -0500

    Basic Canvas implementation

--- gameOutput.js
+++ gameOutput.js
@@ -8,73 +8,64 @@ const triangleVert = [new Vec(1,0), new Vec(-1,0), new Vec(0,-1)];
 const squareVert = [new Vec(1,1), new Vec(-1,1), new Vec(-1,-1), new Vec(1,-1)];
 
 
-function myCanvas() {
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-
-  //ctx.drawImage(  ,10,10);
-}
-
-function drawScreen(){
-  let line = [];
+function drawScreen() {
+  var c = document.getElementById("myCanvas").getContext("2d");
+  c.translate(...[400,400]);
+  c.fillStyle = '#ff0000';
+ c.strokeStyle = '#ff00ff';
+ c.lineWidth = 5
 
   for(let ho in hexObjects){
     if(hexObjects.hasOwnProperty(ho)){
-      line.push(drawPolygon(hexVert, getXYfromHex(hexObjects[ho].hex),new Vec(0,0), hexSize, 1, "#25202D", "#120F22"  ));
+      console.log("draw hex");
+      drawPoly(c, hexVert, getXYfromHex(hexObjects[ho].hex), hexSize, 1, "#25202D", "#120F22"  );
 
       if( hexObjects[ho].terain == "nebula"){
-        line.push(drawPolygon(hexVert, getXYfromHex(hexObjects[ho].hex),new Vec(0,0), hexSize, 2 , "#25202D", "grey"));
+        drawPoly(c, hexVert, getXYfromHex(hexObjects[ho].hex), hexSize, 2 , "#25202D", "grey");
       }
 
       if( hexObjects[ho].station){
-        line.push(drawPolygon(squareVert, getXYfromHex(hexObjects[ho].hex),new Vec(0,15), 10, 2 , playerColours[hexObjects[ho].station.owner], "white"));
+        drawPoly(c, squareVert, getXYfromHex(hexObjects[ho].hex), 10, 2 , playerColours[hexObjects[ho].station.owner], "white");
       }
     }
   }
 
   for(let sh in shipArray){
-    line.push(drawPolygon(triangleVert, getXYfromHex(shipArray[sh].location) ,new Vec(0,0), 30,  2 , playerColours[shipArray[sh].owner], playerColours[shipArray[sh].owner]));
-    line.push(drawText(`${shipArray[sh].shield},${shipArray[sh].hull}`, getXYfromHex(shipArray[sh].location) ,new Vec(-15,0)));
+    drawPoly(c, triangleVert, getXYfromHex(shipArray[sh].location), 30,  2 , playerColours[shipArray[sh].owner], playerColours[shipArray[sh].owner]);
+  //  line.push(drawText(`${shipArray[sh].shield},${shipArray[sh].hull}`, getXYfromHex(shipArray[sh].location) ,new Vec(-15,0)));
   }
 
   if(selected.hex){
     for (let p in possibleMoves){
-    line.push(drawPolygon(hexVert, getXYfromHex(possibleMoves[p]),new Vec(0,0), hexSize -5, 3 , "green"));
+    drawPoly(c, hexVert, getXYfromHex(possibleMoves[p]), hexSize -5, 3 , "green");
     }
     for (let p in possibleAttacks){
-      line.push(drawPolygon(hexVert, getXYfromHex(possibleAttacks[p]),new Vec(0,0), hexSize -5, 3 , "purple"));
+      drawPoly(c, hexVert, getXYfromHex(possibleAttacks[p]), hexSize -5, 3 , "purple");
     }
-    line.push(drawPolygon(hexVert, getXYfromHex(selected.hex),new Vec(0,0), hexSize -5, 3 , selectedColour[selected.state]));
+    drawPoly(c, hexVert, getXYfromHex(selected.hex),new Vec(0,0), hexSize -5, 3 , selectedColour[selected.state]);
   }
 
-  reScale();
-   document.querySelector("#board").innerHTML = line;
-}
 
-function reScale(){
-document.querySelector("#board").setAttribute("viewBox",`${screenOffset.x} ${screenOffset.y} ${screenSize/scale} ${screenSize/scale}`);
+c.translate(...[-400,-400]);
+
 }
 
 function drawMenu(){
-  let line = [];
-  if (selected.state == 3){
-    line.push(`<polygon points='20,20 60,20 60,60 20,60' stroke='white' stroke-width=2' fill='white' />`);
-  }
-  document.querySelector("#menu").innerHTML = line;
+  if (selected.state == 3){    }
 }
 
-function coord_as_string(a){ return `${a.x},${a.y} `; }
+function drawPoly(c, pointVec, center = new Vec(0,0), scale = 50, width, sColor, fColor){
+if(width){c.lineWidth = width};
+if(width){c.strokeStyle = sColor};
+if(width){c.fillStyle = fColor};
 
-function drawPolygon( pointVec, center, offset= new Vec(0,0), size=hexSize, stroke=2, colour="white", fill="none") {
-  let positions = "";
-  let location = center.add(offset );
-  for(let i=0; i<pointVec.length; i++){
-    positions = positions.concat(coord_as_string(pointVec[i].scale(size).add(location)));
-  }
-  return `<polygon points='${positions}' stroke='${colour}' stroke-width='${stroke}' fill='${fill}' />` ;
+c.moveTo(pointVec[0].x, pointVec[0].y);
+c.beginPath();
+for (let points in pointVec){
+  let {x,y} = pointVec[points].scale(scale).add(center);
+  c.lineTo(x, y)
 }
-
-function drawText( text, center, offset= new Vec(0,0), size=20, fill="white") {
-  let pos = center.add(offset)
-  return  `<text x="${pos.x}" y="${pos.y}" fill="${fill}" style="font:bold ${size}px sans-serif"> ${text} </text>`
+  c.closePath();
+  c.stroke();
+  if(fColor){c.fill();}
 }
