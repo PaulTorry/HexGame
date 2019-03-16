30db8240da746f6f22a9a35a2165ace2772dde09
Paul T <PaulTorry@users.noreply.github.com>
2019-03-15 23:19:11 -0500

    Number of players / Human AI

--- src/gameSetup.js
+++ src/gameSetup.js
@@ -10,9 +10,9 @@ let state = {};
 
 let sel = {state:0, attacks:[], menu:[], moves:[]}
 
-state = setup(6, boardSize, 2)
+state = setup(3, boardSize, 3, false)
 
-function setup(numPlayers, boardSize = 8, numHumans = numPlayers){
+function setup(numPlayers, boardSize = 8, numHumans = numPlayers, playersTogether = false){
   let tiles = new Map();
   let playerData = [];
   let baseArray = [];
@@ -29,20 +29,39 @@ function setup(numPlayers, boardSize = 8, numHumans = numPlayers){
     tiles.set(hex.id, buildingHex);
   }
 
+  let playerlist = []
+
+  // TODO Fix this
+
+  for(let i = 0; i < numPlayers; i++){  playerlist.push("AI")  }
+  if(playersTogether){
+    for(let i = 0; i < numHumans; i++){ playerlist[i] = "human";  }
+  } else {
+    for(let i = 0; i < numHumans; i++){ playerlist[Math.floor(i*numPlayers/numHumans)] = "human";  }
+  }
+
+  // for(let i = 0; i < numPlayers; i++){
+  //   if(playersTogether){
+  //     playerlist[i] = i<numHumans ? "human" : "AI";
+  //   } else {
+  //     playerlist[i] = Math.floor(i%((numPlayers-1)/numHumans)) ? "AI" : "human";
+  //   }
+  //   console.log(playerlist[i], i);
+  // }
+
+
+
   for(let i = 0; i < numPlayers; i++){
-    let playerType = i%(numPlayers/numHumans) ? "AI" : "human";
-    console.log(playerType, i);
-    //  let playerType = i<numHumans ? "human" : "AI";
-    let hexloc = Hex.getUnitHexFromXY(   new Vec(
-      Math.sin(2*Math.PI*i/numPlayers), Math.cos(2*Math.PI*i/numPlayers )
-    ).scale(boardSize/1)  )
+    let angle = 2*Math.PI*i/numPlayers
+
+    let hexloc = Hex.getUnitHexFromXY(new Vec(Math.sin(angle), Math.cos(angle)).scale(boardSize/1))
 
     baseArray.push({type:"planet", owner:i, hex: hexloc, territory:hexloc.secondNeighboursInclusive});
-    playerData.push({type: playerType, money:5, tech:{}, capital:hexloc, viewMask:makeNewViewMask(tiles)})
+    playerData.push({type: playerlist[i], money:5, tech:{}, capital:hexloc, viewMask:makeNewViewMask(tiles)})
     shipArray.push({"type":"scoutShip","hull":1,"shield":2,"attack":2,"retaliate":1,
       view:2, "maxMove":4,"moved":false,"attacked":false, hex:hexloc,"owner":i, range:1})
     tiles.set(hexloc.id, {hex: hexloc, terrain:"planet", station:null});
   }
 
-  return {numPlayers:numPlayers, playerTurn:0, shipArray:shipArray, tiles:tiles,   playerData:playerData, baseArray:baseArray}
+  return {numPlayers:numPlayers, playerTurn:0, shipArray:shipArray, tiles:tiles, playerData:playerData, baseArray:baseArray}
 }
