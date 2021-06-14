c4f24244713e1b85a190dcb8c0f1fe0636f17d6e
Paul T <PaulTorry@users.noreply.github.com>
2021-06-13 21:48:45 -0400

    Update buildBarLogic.js
    Reformat as array of checks

--- src/buildBarLogic.js
+++ src/buildBarLogic.js
@@ -9,67 +9,34 @@ state, territoryState, shipState, getShipOnHex, data
 /* eslint-disable no-unused-vars */
 
 function makeBuildBar (hex) {
-  // console.log("makemenu");
   const base = state.baseArray.find(b => b.hex.compare(hex))
   const tile = state.tiles.get(hex.id)
-  const ship = getShipOnHex(hex) // shipArray.find(e => e.location.compare(hex));
-  // console.log("makeBuildBar",base, tile, ship, hex.id);
-  return data.thingList.filter(pos => { // make a map to include failure point
-    //  console.log("pos ", pos);
-    if (pos.price > state.playerData[state.playerTurn].money) return false
-    // console.log(" price  success");
+  const ship = getShipOnHex(hex)
+  const checks = [
+    (a) => a.price > state.playerData[state.playerTurn].money,
+    (a) => a.terrain.length !== 0 && !a.terrain.find(e => e === tile.terrain),
+    // (a) => a.base && !(state.baseArray.filter(b => { return b.hex.compare(hex) && (b.owner === state.playerTurn) })[0]),
+    (a) => a.territoryState && a.territoryState > territoryState(hex),
+    (a) => a.resource && (a.resource !== tile.resource),
+    (a) => a.inhabitedPlanet && !base,
+    (a) => a.shipState === 'noShip' && ship,
+    (a) => a.shipState === 'ownPresent' && (!ship || ship.owner !== state.playerTurn),
+    (a) => a.shipState === 'ownPresentUnmoved' && (!ship || ship.owner !== state.playerTurn || ship.moved === true),
+    (a) => a.shipState === 'noEnemy' && getShipOnHex(hex) && shipState(hex) < 1,
+    (a) => a.tech && !state.playerData[state.playerTurn].tech[a.tech],
+    // (a) => a.thingPresent && a.thingPresent.find(t => t === 'navBeacon') && !tile.navBeacon,
+    (a) => a.nextTo && !hex.neighbours.filter(x => x.mag < state.boardSize).find((x) => state.tiles.get(x.id).terrain === a.nextTo),
+    (a) => a.thing === 'navBeacon' && tile.navBeacon && tile.navBeacon.owner === state.playerTurn,
+    (a) => a.thing === 'navAsteroid' && tile.navBeacon && tile.navBeacon.owner === state.playerTurn,
+    (a) => a.thing === 'navNebula' && tile.navBeacon && tile.navBeacon.owner === state.playerTurn,
+    (a) => a.type === 'industry' && tile.station,
+    (a) => a.thing === 'inhabitedPlanet' && base && base.owner === state.playerTurn
+  ]
 
-    if (pos.terrain.length !== 0 &&
-       !pos.terrain.find(e => e === tile.terrain)) return false
-    // console.log(" terrain  success");
-
-    if (pos.territoryState && pos.territoryState > territoryState(hex)) return false
-    // console.log(" territoryState  success");
-
-    if (pos.resource && pos.resource !== tile.resource) return false
-
-    if (pos.base && !state.baseArray.filter(b => { return b.hex.compare(hex) && b.owner === state.playerTurn })[0]) {
-      return false
-    }
-    // console.log(" base  success");
-
-    if (pos.inhabitedPlanet) { if (!base) return false }
-    // console.log(" inhabitedPlanet  success");
-
-    if (pos.shipState === 'noShip' && ship) return false
-    // console.log(" noShip  success");
-
-    if (pos.shipState === 'ownPresent' && (!ship || ship.owner !== state.playerTurn)) return false
-    // console.log(" ownPresent  success");
-
-    if (pos.shipState === 'ownPresentUnmoved' && (!ship || ship.owner !== state.playerTurn || ship.moved === true)) {
-      return false
-    }
-    // console.log(" ownPresentUnmoved  success");
-
-    if (pos.shipState === 'noEnemy' && getShipOnHex(hex) && shipState(hex) < 1) return false
-    // console.log(" noEnemy  success");
-
-    if (pos.tech && !state.playerData[state.playerTurn].tech[pos.tech]) return false
-    // console.log(" tech  success");
-    // if(pos.tech && pos.tech.length > 0){
-    //   for (let t of pos.tech){ if(!state.playerData[state.playerTurn].tech[t]) return false; }
-    // }
-
-    if (pos.thingPresent && pos.thingPresent.find(t => t === 'navBeacon') && !tile.navBeacon) return false
-    // console.log(" thingPresent  success");
-    // Self check
-
-    if (pos.nextTo && !hex.neighbours.filter(x => x.mag < state.boardSize).find((x) => state.tiles.get(x.id).terrain === pos.nextTo)) return false
-
-    if (pos.thing === 'navBeacon') { if (tile.navBeacon && tile.navBeacon.owner === state.playerTurn) return false }
-    if (pos.thing === 'navAsteroid') { if (tile.navBeacon && tile.navBeacon.owner === state.playerTurn) return false }
-    if (pos.thing === 'navNebula') { if (tile.navBeacon && tile.navBeacon.owner === state.playerTurn) return false }
-
-    if (pos.type === 'industry' && tile.station) return false
-
-    if (pos.thing === 'inhabitedPlanet' && base && base.owner === state.playerTurn) return false
-
-    return true
+  return data.thingList.filter(pos => {
+    const checklist = checks.map(check => check(pos))
+    const result = checklist.reduce((p, c, i, a) => p || c, false)
+    console.log(pos.name, checklist, result)
+    return !result
   }).map(pos => pos.thing)
 }
